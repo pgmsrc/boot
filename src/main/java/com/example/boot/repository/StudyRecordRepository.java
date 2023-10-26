a289ec373b103e95d68122be112cebae7859d924
pgmsrc <pgmsrc@gmail.com>
2023-10-26 21:11:58 +1100

    Init

---
+++ src/main/java/com/example/boot/repository/StudyRecordRepository.java
@@ -0,0 +1,7 @@
+package com.example.boot.repository;
+
+import com.example.boot.entity.Study_record;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRecordRepository extends JpaRepository<Study_record, Integer> {
+}
